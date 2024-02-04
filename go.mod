cec9657d8c2038880f8f45ea2d5b6c4b9a8a0bb5
Tyler Barnes <tylerdbarnes@gmail.com>
2024-02-04 01:09:34 -0800

    my first bubbletea tui

---
+++ go.mod
@@ -0,0 +1,46 @@
+module github.com/TylerBarnes/deez
+
+go 1.21.5
+
+require (
+	github.com/charmbracelet/bubbletea v0.25.0
+	github.com/lotusdblabs/lotusdb/v2 v2.0.0
+)
+
+require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/bwmarrin/snowflake v0.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/charmbracelet/bubbles v0.18.0 // indirect
+	github.com/charmbracelet/lipgloss v0.9.1 // indirect
+	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
+	github.com/dgraph-io/badger/v4 v4.1.0 // indirect
+	github.com/dgraph-io/ristretto v0.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/gofrs/flock v0.8.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/glog v1.1.1 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.4 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.6 // indirect
+	github.com/rosedblabs/diskhash v0.0.0-20230825144203-2d051ecdb9a5 // indirect
+	github.com/rosedblabs/wal v1.3.4-0.20230903095514-51f5de6bb0d3 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	go.etcd.io/bbolt v1.3.7 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/term v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,223 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+
+	"github.com/lotusdblabs/lotusdb/v2"
+)
+
+var db *lotusdb.DB
+
+type model struct {
+	Choices     []string
+	Cursor      int
+	Selected    map[int]struct{}
+	CurrentView string
+	textInput   textinput.Model
+}
+
+func (m *model) toJSON() ([]byte, error) {
+	data, err := json.Marshal(m)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func modelFromJSON(data []byte) (*model, error) {
+	var m model
+	err := json.Unmarshal(data, &m)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &m, nil
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+
+	if m.CurrentView == "list" {
+		updateList(&m, msg)
+	} else if m.CurrentView == "add" {
+		updateAdd(&m, msg)
+		m.textInput, cmd = m.textInput.Update(msg)
+		return m, cmd
+	}
+
+	modelJSON, err := m.toJSON()
+	if err != nil {
+		panic(err)
+	}
+	db.Put([]byte("model"), modelJSON, nil)
+
+	if m.CurrentView == "" {
+		m.CurrentView = "list"
+	}
+
+	return m, nil
+}
+
+func updateAdd(m *model, msg tea.Msg) tea.Model {
+	m.textInput.Focus()
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "tab":
+			m.CurrentView = "list"
+			return m
+		case "enter":
+			m.Choices = append([]string{m.textInput.Value()}, m.Choices...)
+			m.textInput.Reset()
+			m.Cursor = 0
+			m.CurrentView = "list"
+			return m
+		}
+	}
+
+	return m
+}
+
+func updateList(m *model, msg tea.Msg) tea.Model {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "tab":
+			m.CurrentView = "add"
+			return m
+		case "k":
+			if m.Cursor > 0 {
+				m.Cursor--
+			} else {
+				m.Cursor = len(m.Choices) - 1
+			}
+		case "j":
+			if m.Cursor < len(m.Choices)-1 {
+				m.Cursor++
+			} else {
+				m.Cursor = 0
+			}
+		case "enter", " ":
+			_, ok := m.Selected[m.Cursor]
+			if ok {
+				delete(m.Selected, m.Cursor)
+			} else {
+				m.Selected[m.Cursor] = struct{}{}
+			}
+		}
+
+	}
+
+	return m
+}
+
+func listView(m model) string {
+	s := "DO IT!\n\n"
+
+	for i, choice := range m.Choices {
+		cursor := "  "
+
+		if m.Cursor == i {
+			cursor = "> "
+		}
+
+		cursor += choice
+
+		if _, ok := m.Selected[i]; ok {
+			cursor += " x"
+		} else {
+			cursor += "  "
+		}
+
+		cursor += "\n"
+
+		s += cursor
+	}
+
+	return s
+}
+
+func addView(m model) string {
+	return m.textInput.View()
+}
+
+func (m model) View() string {
+	switch m.CurrentView {
+	case "add":
+		return addView(m)
+	case "list":
+		return listView(m)
+
+	}
+
+	return listView(m)
+}
+
+func initialModel() model {
+	storedModel, err := db.Get([]byte("model"))
+	if err != nil && err != lotusdb.ErrKeyNotFound {
+		panic(err)
+	}
+
+	ti := textinput.New()
+	ti.Placeholder = "Do the thing"
+	ti.CharLimit = 156
+	ti.Width = 20
+
+	if storedModel != nil {
+		restoredModel, err := modelFromJSON(storedModel)
+		if err != nil {
+			panic(err)
+		}
+		restoredModel.textInput = ti
+		return *restoredModel
+	}
+
+	return model{
+		Choices:     []string{"Do a thing", "do another thing"},
+		Selected:    make(map[int]struct{}),
+		CurrentView: "list",
+		textInput:   ti,
+	}
+}
+
+func main() {
+	options := lotusdb.DefaultOptions
+	options.DirPath = "exdb"
+	ldb, err := lotusdb.Open(options)
+
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		_ = db.Close()
+	}()
+
+	db = ldb
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
